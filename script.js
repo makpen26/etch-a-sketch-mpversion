4a696b211507e3ad63e99e6f382467873d4c8502
Makara Pen <129983771+makpen26@users.noreply.github.com>
2025-09-26 22:24:33 -0500

    Support multiple round of sketching without needing to reload the browser
    User can reset the dimension of the grid after playing
    the first round by clicking on the button. Once cliked,
    they will see a new grid with the newly input dimension
    and sketching begins.

--- script.js
+++ script.js
@@ -1,62 +1,57 @@
-const size = 30;
+let size = 0;
 const squareDimen = "39px";
 
 
-const mainDivGrid = document.createElement("div");
-mainDivGrid.setAttribute('id', 'main-div');
-document.body.appendChild(mainDivGrid);
+const btnToClearAndRestart = document.querySelector("#button");
+btnToClearAndRestart.addEventListener('click', () => {
+    const mainDivGrid = document.createElement("div");
+    mainDivGrid.setAttribute('id', 'main-div');
+    document.body.appendChild(mainDivGrid);
 
 
-for (let i = 0; i < size; i++) {
-    const divForGrids = document.createElement("div");
-    const selectorStringId = `container${i}`;
-    divForGrids.setAttribute(`id`, selectorStringId);
+    size = prompt("Enter square's dimension for this grid");
 
-    mainDivGrid.appendChild(divForGrids);
+    for (let i = 0; i < size; i++) {
+        const divForGrids = document.createElement("div");
+        const selectorStringId = `container${i}`;
+        divForGrids.setAttribute(`id`, selectorStringId);
 
-    for (let j = 0; j < size; j++) {
-        const individualSquareDiv = document.createElement("div");
-        const selectorStringClass = `eachDivSquare${j}`;
-        individualSquareDiv.classList.add(selectorStringClass);
-        individualSquareDiv.classList.add(`square`);
+        mainDivGrid.appendChild(divForGrids);
 
-        individualSquareDiv.style.border = "1px solid black";
-        individualSquareDiv.style.height = squareDimen;
-        individualSquareDiv.style.width = squareDimen;
+        for (let j = 0; j < size; j++) {
+            const individualSquareDiv = document.createElement("div");
+            const selectorStringClass = `eachDivSquare${j}`;
+            individualSquareDiv.classList.add(selectorStringClass);
+            individualSquareDiv.classList.add(`square`);
 
-        divForGrids.append(individualSquareDiv);
+            individualSquareDiv.style.border = "1px solid black";
+            individualSquareDiv.style.height = squareDimen;
+            individualSquareDiv.style.width = squareDimen;
+
+            divForGrids.appendChild(individualSquareDiv);
+        }
+
+        divForGrids.style.display = "flex";
+
+    }
+    
+    function random(number) {
+        return Math.floor(Math.random() * (number + 1));
     }
 
-    divForGrids.style.display = "flex";
-}
+    function changeBackgroundColor(event) {
+        const randColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+        event.target.style.backgroundColor = randColor;
+    }
 
-
-
-
-
-
-const entireGridBoard = document.querySelectorAll('.square');
-entireGridBoard.forEach((div) => {
-    div.addEventListener("mouseenter", (event) => {
+    const entireGridBoard = document.querySelectorAll('.square');
+        entireGridBoard.forEach((div) => {
+        div.addEventListener("mouseenter", (event) => {
             changeBackgroundColor(event);
+        });
+    });
+    
+    btnToClearAndRestart.addEventListener('click', () => {
+        document.body.removeChild(mainDivGrid);
     });
 });
-
-
-
-
-
-
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
-}
-
-
-
-
-
-
-function changeBackgroundColor(event) {
-    const randColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-    event.target.style.backgroundColor = randColor;
-}
